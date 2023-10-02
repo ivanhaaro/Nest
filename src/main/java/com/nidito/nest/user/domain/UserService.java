be6452c35e1bab8f2968129f25a2182a8ec47d5b
ivanhaaro <ivanharoliminana@outlook.es>
2023-10-02 21:24:05 +0200

    User service interface added

--- src/main/java/com/nidito/nest/user/domain/UserService.java
+++ src/main/java/com/nidito/nest/user/domain/UserService.java
@@ -2,19 +2,6 @@ package com.nidito.nest.user.domain;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.nidito.nest.user.infrastructure.UserRepository;
-
-@Service
-public class UserService {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    public List<User> getUsers() {
-        return userRepository.findAll();
-    }
-    
+public interface UserService {
+    List<User> getUsers();
 }
