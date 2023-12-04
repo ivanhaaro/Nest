ade8d061f4198e33c9df7e2e59ed4cc3f0fcbbda
ivanhaaro <ivanharoliminana@gmail.com>
2023-12-04 12:56:36 +0100

    Create letter test

---
+++ src/test/java/com/nidito/nest/LetterTests.java
@@ -0,0 +1,75 @@
+package com.nidito.nest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Date;
+import java.util.Optional;
+
+import com.nidito.nest.letter.application.LetterController;
+import com.nidito.nest.letter.domain.entity.LetterDto;
+import com.nidito.nest.user.domain.entity.User;
+import com.nidito.nest.user.infrastructure.UserRepository;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LetterTests {
+
+    @Autowired
+    private LetterController controller;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(controller);
+    } 
+
+    @Test
+    void creatingNote() throws Exception {
+        
+        LetterDto letter = new LetterDto();
+        letter.setTitle("title");
+        letter.setText("text");
+        letter.setOpened(false);
+        letter.setDate(new Date());
+
+        User user = getDefaultUser();
+
+        letter.setOriginUserId(user.getId());
+        letter.setReceiverUserId(user.getId());
+
+        ResponseEntity<LetterDto> res = controller.createLetter(letter);
+
+        mvc.perform(get("/letter/" + res.getBody().getId())).andExpect(status().isOk());
+    }
+
+    private User getDefaultUser() {
+
+        Optional<User> res = userRepository.findByUsername("ivanharo");
+        if (res.isPresent()) return res.get();
+
+        User user = new User(); 
+        user.setName("Ivan");
+        user.setLastname("Haro");
+        user.setMail("ivanharo@gmail.com");
+        user.setUsername("ivanharo");
+        user.setPassword("12345");
+
+        return userRepository.save(user);
+    }
+    
+}
--- src/test/java/com/nidito/nest/NestApplicationTests.java
+++ src/test/java/com/nidito/nest/NestApplicationTests.java
@@ -1,10 +1,17 @@
 package com.nidito.nest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class NestApplicationTests {
+
+	@Autowired
+    private MockMvc mvc;
 
 	@Test
 	void contextLoads() {
